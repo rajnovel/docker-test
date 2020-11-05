0fa47eea097a2b5392d396b83b0312f6ae344f11
rajnovel <raj.novel@gmail.com>
2020-11-05 19:41:09 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f /srv/server.jar ]
+then
+    wget "https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar" -O /srv/server.jar
+fi
+
+if [ ! -f /srv/eula.txt ]
+then
+    echo "eula=true" > /srv/eula.txt
+fi
+
+cd /srv/ && java -Xms1024M -Xmx1024M -jar server.jar nogui
